92d2edbdbf62504102aba2f91230b608f0f4f2f0
egormelnikov <egor.melnikov@effective-soft.com>
2022-04-19 13:48:56 +0300

    kubernetes

--- internal/config/config.go
+++ internal/config/config.go
@@ -6,12 +6,13 @@ import "github.com/caarlos0/env/v6"
 // Config struct to postgres config env
 type Config struct {
 	DB             string `env:"DB" envDefault:"postgres"`
-	User           string `env:"USER" envDefault:"egormelnikov"`
-	Password       string `env:"PASSWORD" envDefault:"54236305"`
-	Host           string `env:"HOST" envDefault:"postgres"`
-	PortPostgres   int    `env:"PORT" envDefault:"5432"`
-	DBNamePostgres string `env:"DBNAMEPOSTGRES" envDefault:"egormelnikovdb"`
+	User           string `env:"POSTGRES_USER" envDefault:"egormelnikov"`
+	Password       string `env:"POSTGRES_PASSWORD" envDefault:"54236305"`
+	Host           string `env:"POSTGRES_HOST" envDefault:"postgres"`
+	PortPostgres   string `env:"POSTGRES_PORT" envDefault:"5432"`
+	DBNamePostgres string `env:"POSTGRES_DB" envDefault:"egormelnikov"`
 	DBURL          string `env:"DBURL" envDefault:""`
+	PricePort      string `env:"PRICE_PORT" envDefault:"8089"`
 }
 
 // New contract config
